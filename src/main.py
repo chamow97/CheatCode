be61f809cb89f29a2fea0d237df532cba0c12f29
Chandramowli <jchandramowli15039@it.ssn.edu.in>
2020-01-16 09:37:41 +0530

    Add Constants for strings

--- src/main.py
+++ src/main.py
@@ -5,7 +5,13 @@ from selenium import webdriver
 
 from constants.constants import LEET_CODE_LOGIN_URL, USERNAME_HTML_ELEMENT_NAME, PASSWORD_HTML_ELEMENT_NAME, \
     ACTUAL_PREMIUM_PASSWORD, ACTUAL_PREMIUM_USERNAME, LEET_CODE_PROBLEM_SET, SUBMIT_BUTTON_CLASS_NAME, \
-    COMPANY_SECTION_ID, ANCHOR_ELEMENT, COMPANY_PREFIX, HREF_ATTRIBUTE, LEET_CODE_HOME_URL, SUBSCRIBE
+    COMPANY_SECTION_ID, ANCHOR_ELEMENT, COMPANY_PREFIX, HREF_ATTRIBUTE, LEET_CODE_HOME_URL, SUBSCRIBE, SIGN_IN_SLEEP, \
+    SIGN_IN_PAGE_LOAD_SLEEP, QUESTION_NAVIGATION_TIME_SLEEP, QUESTION_LOAD_TIME_SLEEP, T_BODY_HTML_ELEMENT, \
+    COMPANY_WISE_QUESTION_LIST_HTML_CLASS, TR_HTML_ELEMENT, TD_HTML_ELEMENT, TXT_EXTENSION, URL_SPLITTER, \
+    VALUE_HTML_ATTRIBUTE, QUESTION_ID_NAME_SEPARATOR, PREMIUM_QUESTION_LIST_ID, ORDERED_LIST_HTML_ELEMENT, \
+    LISTED_ITEM_HTML_ELEMENT, LIST_SEPARATOR, QUESTION_DESCRIPTION_CLASS_NAME, DIV_HTML_ELEMENT, HTML_EXTENSION, \
+    HTML_TEMPLATE_START, HTML_TEMPLATE_END, OUTPUT_PATH, COMPANIES_LIST_NAME, PREMIUM_QUESTIONS_NAME, \
+    PREMIUM_QUESTIONS_PATH
 
 
 # login into Leetcode to get the dumps
@@ -15,7 +21,11 @@ def sign_in_user(driver, url):
     password = driver.find_element_by_name(PASSWORD_HTML_ELEMENT_NAME)
     username.send_keys(ACTUAL_PREMIUM_USERNAME)
     password.send_keys(ACTUAL_PREMIUM_PASSWORD)
-    time.sleep(65)
+    # change this time based on whether you get recaptcha
+    # :( we have to manually check whether it asks for recaptcha if yes, click on signin button
+    # and recaptcha it before this time ends, else you signout and comeback to this login page
+    # and manually enter the credentials and wait, will find a better way to handle this in future
+    time.sleep(SIGN_IN_SLEEP)
     sign_in_button = driver.find_element_by_class_name(SUBMIT_BUTTON_CLASS_NAME)
     sign_in_button.click()
 
@@ -25,7 +35,7 @@ def get_logged_in_dumps(url_of_dump_needed):
     driver = webdriver.Firefox()
     sign_in_user(driver, LEET_CODE_LOGIN_URL)
     driver.get(url_of_dump_needed)
-    time.sleep(5)
+    time.sleep(SIGN_IN_PAGE_LOAD_SLEEP)
     html_page_source = driver.page_source
     driver.close()
     return html_page_source
@@ -44,53 +54,55 @@ def get_company_list(source):
 def visit_pages_scrap_question_url(companies_list):
     driver = webdriver.Firefox()
     sign_in_user(driver, LEET_CODE_LOGIN_URL)
-    time.sleep(10)
+    time.sleep(QUESTION_NAVIGATION_TIME_SLEEP)
     for company in companies_list:
         driver.get(LEET_CODE_HOME_URL + company)
-        time.sleep(16)
+        time.sleep(QUESTION_LOAD_TIME_SLEEP)
         page_src = driver.page_source
         soup = bs4.BeautifulSoup(page_src)
-        problems_table = soup.find("tbody", {"class": "reactable-data"})
-        rows = problems_table.find_all("tr")
-        with open("../output/" + company.split("/")[2] + ".txt", 'w') as f2:
+        problems_table = soup.find(T_BODY_HTML_ELEMENT, {"class": COMPANY_WISE_QUESTION_LIST_HTML_CLASS})
+        rows = problems_table.find_all(TR_HTML_ELEMENT)
+        with open(OUTPUT_PATH + company.split(URL_SPLITTER)[2] + TXT_EXTENSION, 'w') as f2:
             for row in rows:
-                question_data = row.find_all("td")
-                question_id = question_data[1].attrs["value"]
-                question_name = question_data[2].attrs["value"]
-                f2.write(question_id + ":" + question_name + "\n")
+                question_data = row.find_all(TD_HTML_ELEMENT)
+                question_id = question_data[1].attrs[VALUE_HTML_ATTRIBUTE]
+                question_name = question_data[2].attrs[VALUE_HTML_ATTRIBUTE]
+                f2.write(question_id + QUESTION_ID_NAME_SEPARATOR + question_name + "\n")
             f2.close()
     driver.close()
-    return []
 
 
 def store_premium_question_links(driver):
     driver.get(LEET_CODE_HOME_URL + SUBSCRIBE)
-    time.sleep(10)
+    time.sleep(QUESTION_NAVIGATION_TIME_SLEEP)
     page_src = driver.page_source
     soup = bs4.BeautifulSoup(page_src)
-    list_of_questions = soup.find(id='pq-list').find_all_next("ol")[0].find_all_next("li")
-    with open("../output/premium_questions.txt", 'w') as f3:
+    list_of_questions = soup.find(id=PREMIUM_QUESTION_LIST_ID).find_all_next(ORDERED_LIST_HTML_ELEMENT)[0]\
+        .find_all_next(LISTED_ITEM_HTML_ELEMENT)
+    with open(OUTPUT_PATH + PREMIUM_QUESTIONS_NAME, 'w') as f3:
         for question in list_of_questions:
-            question_data = question.find("a")
-            f3.write(question_data.attrs["href"] + ",")
+            question_data = question.find(ANCHOR_ELEMENT)
+            f3.write(question_data.attrs[HREF_ATTRIBUTE] + LIST_SEPARATOR)
         f3.close()
 
 
 def open_premium_questions_and_fetch_content(driver):
-    with open("../output/premium_questions.txt", 'r') as premium_questions_file:
+    with open(OUTPUT_PATH + PREMIUM_QUESTIONS_NAME, 'r') as premium_questions_file:
         questions = premium_questions_file.readlines()
         premium_questions_file.close()
     questions_list = []
     if len(questions) > 0:
-        questions_list = questions[0].split(",")
+        questions_list = questions[0].split(LIST_SEPARATOR)
     sign_in_user(driver, LEET_CODE_LOGIN_URL)
-    time.sleep(10)
+    time.sleep(QUESTION_NAVIGATION_TIME_SLEEP)
     for question in questions_list:
         driver.get(LEET_CODE_HOME_URL + question)
-        time.sleep(10)
-        question_description = bs4.BeautifulSoup(driver.page_source).find_all("div", {"class": "description__24sA"})
-        with open("../output/premiumq/" + question.split("/")[2] + ".html", 'w') as premium_file:
-            premium_file.write("<html><body>" + str(question_description[0]) + "</body></html>")
+        time.sleep(QUESTION_NAVIGATION_TIME_SLEEP)
+        question_description = bs4.BeautifulSoup(driver.page_source)\
+            .find_all(DIV_HTML_ELEMENT, {"class": QUESTION_DESCRIPTION_CLASS_NAME})
+        with open(OUTPUT_PATH + PREMIUM_QUESTIONS_PATH + question.split(URL_SPLITTER)[2] + HTML_EXTENSION, 'w') \
+                as premium_file:
+            premium_file.write(HTML_TEMPLATE_START + str(question_description[0]) + HTML_TEMPLATE_END)
             premium_file.close()
 
 
@@ -103,25 +115,29 @@ def fetch_premium_questions():
     driver.close()
 
 
+def fetch_company_wise_questions():
+    should_fetch_company_list = False
+    if should_fetch_company_list:
+        page_source = get_logged_in_dumps(LEET_CODE_PROBLEM_SET)
+        company_list = get_company_list(page_source)
+        with open(OUTPUT_PATH + COMPANIES_LIST_NAME, 'w') as f:
+            for item in company_list:
+                f.write("%s," % item)
+            f.close()
+    with open(OUTPUT_PATH + COMPANIES_LIST_NAME, 'r') as f:
+        companies = f.readlines()
+        f.close()
+    company_list = []
+    if len(companies) > 0:
+        company_list = companies[0].split(LIST_SEPARATOR)
+    visit_pages_scrap_question_url(company_list)
+
+
 if __name__ == '__main__':
     should_fetch_company_wise_questions = False
     should_fetch_premium_questions = True
     if should_fetch_company_wise_questions:
-        should_fetch_company_list = False
-        if should_fetch_company_list:
-            page_source = get_logged_in_dumps(LEET_CODE_PROBLEM_SET)
-            company_list = get_company_list(page_source)
-            with open('../output/companies.txt', 'w') as f:
-                for item in company_list:
-                    f.write("%s," % item)
-                f.close()
-        with open('../output/companies.txt', 'r') as f:
-            companies = f.readlines()
-            f.close()
-        company_list = []
-        if len(companies) > 0:
-            company_list = companies[0].split(',')
-        get_company_wise_questions = visit_pages_scrap_question_url(company_list)
+        fetch_company_wise_questions()
 
     if should_fetch_premium_questions:
         fetch_premium_questions()
